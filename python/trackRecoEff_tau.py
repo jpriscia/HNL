4f6a867b33e5803f2650c3a36854926aea2668f1
jpriscia <jessica.prisciandaro@cern.ch>
2018-03-19 22:10:58 +0100

    add taus samples and fixing problems

--- python/trackRecoEff_tau.py
+++ python/trackRecoEff_tau.py
@@ -42,28 +42,31 @@ def isAncestor(a, p):
             return True
         return False
 
-lines = open("fileList_mu4GeV.txt").read().splitlines()
+#lines = open("fileList_mu2GeV.txt").read().splitlines()
 
 #events = Events(lines)
 ########### for test uncomment this ############
-events = Events(['root://cms-xrd-global.cern.ch//store/user/tomc/heavyNeutrinoAOD/Moriond17/displaced/HeavyNeutrino_lljj_M-2_V-0.00836660026534_mu_onshell_pre2017_leptonFirst_NLO/heavyNeutrino_105.root'])
+events = Events(['root://cms-xrd-global.cern.ch//store/mc/RunIISummer16MiniAODv2/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/80000/163E57C9-7ABE-E611-A73A-0025905B857E.root',
+                 'root://cms-xrd-global.cern.ch//store/mc/RunIISummer16MiniAODv2/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/FA69C320-28C0-E611-8F7A-02163E013A88.root',
+                 'root://cms-xrd-global.cern.ch//store/mc/RunIISummer16MiniAODv2/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/80000/5285C43E-41BF-E611-8034-001C23C0F1F9.root'
+                 ])
 #event = events.__iter__().next()
 
-handleReco, labelReco  = Handle('vector<reco::Track>'),'generalTracks'
-handlePruned, labelPruned  = Handle ('std::vector<reco::GenParticle>'), 'genParticles'
+handleReco, labelReco  = Handle('vector<pat::PackedCandidate>'),'packedPFCandidates'
+handlePruned, labelPruned  = Handle ('vector<reco::GenParticle>'), 'prunedGenParticles'
 
-handleJet, labelJet = Handle('vector<reco::PFJet>'), 'ak4PFJetsCHS'
-#handlePacked, labelPacked  = Handle ("std::vector<pat::PackedGenParticle> "), "packedGenParticles"
+handleJet, labelJet = Handle('vector<pat::Jet>'), 'slimmedJets'
+handlePacked, labelPacked  = Handle ("std::vector<pat::PackedGenParticle> "), "packedGenParticles"
 
 #######OutputTTree###############
-outfile = root_open('tree_lljj.root', 'w')
+outfile = root_open('test.root', 'w')
 
 class GenTree(TreeModel):
-        
-        p_NuGen = FloatCol()
-        pt_NuGen = FloatCol()
-        phi_NuGen = FloatCol()
-        eta_NuGen = FloatCol()
+
+        p_tauGen = FloatCol()
+        pt_tauGen = FloatCol()
+        phi_tauGen = FloatCol()
+        eta_tauGen = FloatCol()
         aperture_Gen = FloatCol()
         p_Gen =  stl.vector(float)
         pt_Gen = stl.vector(float)
@@ -87,11 +90,8 @@ class GenTree(TreeModel):
         vz_Reco  = stl.vector(float)
 
         eta_Reco  = stl.vector(float)
-        etaErr_Reco  = stl.vector(float)
         phi_Reco  = stl.vector(float)
-        phiErr_Reco  = stl.vector(float)
         theta_Reco  = stl.vector(float)
-        thetaErr_Reco  = stl.vector(float)
 
         idx = stl.vector(int)
         pdgId = stl.vector(int)
@@ -99,7 +99,6 @@ class GenTree(TreeModel):
         dx_Gen = stl.vector(float)
         dy_Gen = stl.vector(float)
         dz_Gen = stl.vector(float)
-        algo = stl.vector(int)
         # 'dxy', 'dxyError', 'dz', 'dzError'
         dxy_RecoPV = stl.vector(float)
         dxyErr_RecoPV = stl.vector(float)
@@ -113,8 +112,6 @@ class GenTree(TreeModel):
         electrons  = IntCol()
         neutrals  = IntCol()
         charged   = IntCol()
-        dMeson = IntCol()
-
         pt_Jet = FloatCol()
         eta_Jet = FloatCol()
         phi_Jet = FloatCol()
@@ -128,61 +125,65 @@ tree = Tree('gentree', model=GenTree)
 count=1
 
 for evt in events:
+    #print 'new event'
     if count%100==0: print 'processing event ', count
     count+=1
 
     evt.getByLabel(labelPruned, handlePruned)
     evt.getByLabel(labelReco, handleReco)
+    evt.getByLabel(labelPacked, handlePacked)    
     evt.getByLabel(labelJet, handleJet)
 
     # get the product                                                                                                      
     pruned = handlePruned.product()
     recoCand = handleReco.product()
+    packedCand = handlePacked.product()
     recoJets = handleJet.product()
 
     #get the list of maj neutrino - len shoud be always one
-    maj_neutrinos = [pp for pp in pruned if ((abs(pp.pdgId()) == 9900012 or abs(pp.pdgId()) == 9900014 or abs(pp.pdgId()) == 9900016) and pp.isLastCopy())]
-
-    if len(maj_neutrinos) != 1:
-        set_trace()
-        print "we have multiple majorana neutrinos"
+    
+    taus = [pp for pp in pruned if ((abs(pp.pdgId()) == 15) and (pp.isLastCopy()))]
+    #print taus
+    if len(taus) ==0 : 
+        continue
+    elif len(taus)==1 :
+        taus = taus[0]
+    elif len(taus) > 1:
+        taus.sort(key=lambda x: x.pt())
+        taus=taus[-1]
 
     final_particles = []
     final_particles_pi0 = []
-    inter_particles = []
+    #inter_particles = []
     primaryVtx = None
-    dMeson = 0
+    
+    primaryVtx = (taus.vx(),taus.vy(),taus.vz())
+    #print "majorana neutrinos mass: ", maj_n.mass() 
 
-    for maj_n in maj_neutrinos:
-        tree.p_NuGen =   maj_n.p()
-        tree.pt_NuGen =  maj_n.pt()
-        tree.phi_NuGen = maj_n.phi()
-        tree.eta_NuGen = maj_n.eta()
-
-        
-        primaryVtx = (maj_n.vx(),maj_n.vy(),maj_n.vz())
-        #print "majorana neutrinos mass: ", maj_n.mass() 
-
-        #loop on the gen and look at final particles. Pi0 is considered as final particle
-        for p in pruned:
-            if (p.status()==1 and p.isLastCopy() and (p != maj_n) and (p not in final_particles)):
-                if isAncestor(maj_n,p):
-                    if p.mother(0).pdgId()==111 and (p.mother(0).isLastCopy()):
-                        if p.mother(0) not in final_particles :
-                            final_particles.append(p.mother(0))
-                            final_particles_pi0.append(p)
-                    else:
-                        final_particles.append(p)
-
-            #I want to check if there is a D meson as well...
-            if (p.status()!=1 and p.isLastCopy() and (p != maj_n) and (p not in inter_particles)):
-                if isAncestor(maj_n,p):
-                    inter_particles.append(p)
-                    if abs(p.pdgId())==431:
-                        dMeson+=1
+    #loop on the gen and look at final particles. Pi0 is considered as final particle
+    for p in [i for i in pruned]:
+        if ((p.isLastCopy()) and (p != taus) and (p not in final_particles)):
+            if isAncestor(taus,p):
+                if p.mother(0).pdgId()==111 and (p.mother(0).isLastCopy()):
+                    if p.mother(0) not in final_particles :
+                        final_particles.append(p.mother(0))
+                        final_particles_pi0.append(p)
+                else:
+                    final_particles.append(p)
 
     abs_final_part = [ abs(x.pdgId()) for x in final_particles]
+    #print abs_final_part
 
+    if 13 in abs_final_part and 14 in abs_final_part:
+        continue
+    if 11 in abs_final_part and 12 in abs_final_part:
+        continue
+
+    tree.p_tauGen =   taus.p()
+    tree.pt_tauGen =  taus.pt()
+    tree.phi_tauGen = taus.phi()
+    tree.eta_tauGen = taus.eta()
+   
     #save some infos of the generated particles
     for x in final_particles:
         tree.p_Gen.push_back(x.p())
@@ -201,7 +202,7 @@ for evt in events:
         tree.e_Gen.push_back(x.energy())
 
     #this gives the aperture
-    tree.aperture_Gen = max(deltaR(maj_neutrinos[0],i) for i in final_particles)
+    tree.aperture_Gen = max(deltaR(taus,i) for i in final_particles)
 
     charged_gen = [x for x in final_particles if (abs(x.pdgId())==321 or abs(x.pdgId())==211 or abs(x.pdgId())==13 or abs(x.pdgId())==11)]
 
@@ -221,12 +222,14 @@ for evt in events:
         if minEl[1][0][1] not in matchedReco:
             tree.idx.push_back(final_particles.index(minEl[0]))
             tree.dr.push_back(minEl[1][0][0])
-            tree.algo.push_back(minEl[1][0][1].algo())
             tree.dxy_RecoPV.push_back(minEl[1][0][1].dxy())
-            tree.dxyErr_RecoPV.push_back(minEl[1][0][1].dxyError())
             tree.dz_RecoPV.push_back(minEl[1][0][1].dz())
-            tree.dzErr_RecoPV.push_back(minEl[1][0][1].dzError())
-
+            if minEl[1][0][1].hasTrackDetails():
+                tree.dxyErr_RecoPV.push_back(minEl[1][0][1].dxyError())
+                tree.dzErr_RecoPV.push_back(minEl[1][0][1].dzError())
+            else:
+                tree.dxyErr_RecoPV.push_back(-1000)
+                tree.dzErr_RecoPV.push_back(-1000)
             tree.p_Reco.push_back(minEl[1][0][1].p())
             tree.pt_Reco.push_back(minEl[1][0][1].pt())
             tree.px_Reco.push_back(minEl[1][0][1].px())
@@ -238,11 +241,8 @@ for evt in events:
             tree.vz_Reco.push_back(minEl[1][0][1].vz())
 
             tree.eta_Reco.push_back(minEl[1][0][1].eta())
-            tree.etaErr_Reco.push_back(minEl[1][0][1].etaError())
             tree.phi_Reco.push_back(minEl[1][0][1].phi())
-            tree.phiErr_Reco.push_back(minEl[1][0][1].phiError())
             tree.theta_Reco.push_back(minEl[1][0][1].theta())
-            tree.thetaErr_Reco.push_back(minEl[1][0][1].thetaError())
 
             matchedReco.add(minEl[1][0][1])
             genMatchDict.pop(minEl[0])
@@ -261,12 +261,11 @@ for evt in events:
     tree.electrons = abs_final_part.count(11)
     tree.neutrals = abs_final_part.count(130)+abs_final_part.count(310)+abs_final_part.count(111)+abs_final_part.count(16)+abs_final_part.count(14)+abs_final_part.count(12)
     tree.charged = abs_final_part.count(321)+abs_final_part.count(211)+abs_final_part.count(13)+abs_final_part.count(11)
-    tree.dMeson = dMeson
 
     ####### Let's match the jets #######
 
-    jets = [jet for jet in recoJets]
-    matchedJet, _ = bestMatch(maj_neutrinos[0], jets)
+    jets = [jet for jet in recoJets if jet.pt()>20]
+    matchedJet, _ = bestMatch(taus, jets)
 
     #pt eta phi mass btagging discriminator #isolamento
     #B tagging discriminator: look for displaced vertexes and traks in a jet
@@ -276,8 +275,8 @@ for evt in events:
         tree.phi_Jet= matchedJet.phi()
         tree.mass_Jet= matchedJet.mass()
         tree.et_Jet= matchedJet.et()
-        
-        jet_particles =  matchedJet.getPFConstituents()
+       
+        jet_particles =  matchedJet.getJetConstituents()
         deposits = [0]*9
         for prt in jet_particles:
             dr = deltaR(prt, matchedJet)
@@ -285,7 +284,7 @@ for evt in events:
             deposits[idx] += prt.et()
         for i in deposits:
             tree.deposits_Jet.push_back(i)
-    
+       #set_trace()
     else:
         tree.pt_Jet = -1000
         tree.eta_Jet= -1000
--- python/treeDraw.py
+++ python/treeDraw.py
@@ -4,34 +4,34 @@ process = cms.Process("AnalysisProc")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 
-process.source = cms.Source("PoolSource", fileNames =  cms.untracked.vstring('root://cms-xrd-global.cern.ch//store/user/tomc/heavyNeutrinoAOD/Moriond17/displaced/HeavyNeutrino_lljj_M-2_V-0.00836660026534_mu_onshell_pre2017_leptonFirst_NLO/heavyNeutrino_119.root'))
+process.source = cms.Source("PoolSource", fileNames =  cms.untracked.vstring('root://cms-xrd-global.cern.ch//store/mc/RunIISummer16MiniAODv2/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/50000/5AE90EBB-54BF-E611-88AC-5065F381E271.root'))
 #process.source = cms.Source("PoolSource", fileNames =  cms.untracked.vstring('file:/afs/cern.ch/user/j/jpriscia/heavyNeutrino_215.root') )
 #process.source = cms.Source("PoolSource", fileNames =  cms.untracked.vstring('file:heavyNeutrino_235.root'))
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 process.maxEvents = cms.untracked.PSet(
-input = cms.untracked.int32(5)
+input = cms.untracked.int32(100)
 )
 
 process.printDecay = cms.EDAnalyzer("ParticleDecayDrawer",
-    src = cms.InputTag("genParticles"),
+    src = cms.InputTag("prunedGenParticles"),
     printP4 = cms.untracked.bool(False),
     printPtEtaPhi = cms.untracked.bool(False),
     printVertex = cms.untracked.bool(True)
   )
 
 process.printTree = cms.EDAnalyzer("ParticleListDrawer",
-  maxEventsToPrint = cms.untracked.int32(1),
+  maxEventsToPrint = cms.untracked.int32(100),
   printVertex = cms.untracked.bool(False),
-  #printOnlyHardInteraction = cms.untracked.bool(False), 
-  src = cms.InputTag("genParticles")
+  printOnlyHardInteraction = cms.untracked.bool(False), 
+  src = cms.InputTag("prunedGenParticles")
 )
 process.printTree2 = cms.EDAnalyzer("ParticleTreeDrawer",
-                                   src = cms.InputTag("genParticles"),                                                                 
-                                   printP4 = cms.untracked.bool(True),
-                                   printPtEtaPhi = cms.untracked.bool(True),
-                                   printVertex = cms.untracked.bool(True),
-                                   printStatus = cms.untracked.bool(True),
-                                   printIndex = cms.untracked.bool(True),
-                                   #status = cms.untracked.vint32( 3 )
+                                   src = cms.InputTag("prunedGenParticles"),#packedGenParticles                                                         
+                                   printP4 = cms.untracked.bool(False),
+                                   printPtEtaPhi = cms.untracked.bool(False),
+                                   printVertex = cms.untracked.bool(False),
+                                   printStatus = cms.untracked.bool(False),
+                                   printIndex = cms.untracked.bool(False),
+                                   #status = cms.untracked.vint32(1)
                                    )
-process.p = cms.Path(process.printTree2)
+process.p = cms.Path(process.printTree)
